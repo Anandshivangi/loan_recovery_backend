3c11e9f452b8f56b8ddcb3c08f8f41e338e8a40d
aishwarya8989 <aishwaryabeniwal89@gmail.com>
2024-05-22 11:10:01 +0530

    gfd

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
-const express=require('express');
+const express = require('express');
 const app = express();
-const db=require('./DB/db');
-const login_route=require("./master/routes/userLogin")
-PORT=4000;
+const db = require('./DB/db');
+const login_route = require("./master/routes/userLogin")
+const cors=require("cors")
+const rateLimit = require('express-rate-limit');
+require('dotenv').config();
+// require("./db")
 
 app.use(express.json());
-app.use("/api",login_route);
-app.get("/",(req,res)=>{
+app.use("/api", login_route);
+app.get("/", (req, res) => {
     res.send("hello world")
 })
-app.listen(PORT,()=>{
-    console.log(`server is running at port no ${PORT}`);
-    })
+
+
+app.use(cors())
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 500,
+});
+app.use(limiter);
+app.use(express.json({ limit: '5mb' }));
+
+
+app.listen(process.env.port, () => {
+    console.log(`server is running on port  ${process.env.port}`);
+})
