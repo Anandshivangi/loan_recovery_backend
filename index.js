5efc7d8fe6627f953def1659019dc8c6f3b7ed34
aishwarya8989 <aishwaryabeniwal89@gmail.com>
2024-05-22 14:31:06 +0530

    hgf

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 const express = require('express');
 const app = express();
 const db = require('./DB/db');
-// const login_route = require("./master/routes/masterUser")
-const cors=require("cors")
+const login_route = require("./master/routes/masterUser")
+const cors = require("cors")
 const rateLimit = require('express-rate-limit');
+const { operator_auth_route } = require('./operator/routes/operator_auth_route');
 const { admin_auth_route } = require('./admin/routes/admin_auth_route');
 const masterAuthRoute = require('./master/routes/masterUser');
 require('dotenv').config();
 // require("./db")
 
 
-app.use("/master",masterAuthRoute)
+
 app.use(express.json());
-app.use("/api", login_route);
-app.get("/", (req, res) => {
-    res.send("hello world")
-})
+// app.get("/", (req, res) => {
+//   res.send("hello world")
+// })
 
 
 app.use(cors())
@@ -26,9 +26,57 @@ const limiter = rateLimit({
 app.use(limiter);
 app.use(express.json({ limit: '5mb' }));
 
+// routes for master_Admin 
+app.use("/master",masterAuthRoute);
 
-app.use("/admin",admin_auth_route)
+
+
+
+// routes for Admin 
+
+app.use("/admin", admin_auth_route)
+
+
+
+// routes for operator 
+
+app.use("/operator", operator_auth_route);
+
+
+
+// routes for team leader 
+
+
+
+
+// routes for agents
+
+
+
+
+
+app.use(async (err, req, res, next) => {
+
+  if (err.name === "FileValError") {
+    return res.send({ status: "FILE_VAL_ERR", Backend_Error: err.message })
+  }
+
+  console.log(err);
+
+  res.send({ status: "INT_ERR", Backend_Error: err.message });
+
+});
+
+
+app.get("/check", async (req, res) => {
+  res.send({ status: "8421", Backend_Error: "auth microservice is working" });
+});
+
+app.use("*", async (req, res) => {
+  res.send({ status: "6320", Backend_Error: "there  is no route like this" });
+});
+
 
 app.listen(process.env.port, () => {
-    console.log(`server is running on port  ${process.env.port}`);
+  console.log(`server is running on port  ${process.env.port}`);
 })
